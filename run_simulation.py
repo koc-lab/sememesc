dd86e11864be7b59bf937abfc9c73a29b1c78d14
Tuna Özateş <58263711+tunaozates@users.noreply.github.com>
2024-07-26 21:41:38 +0300

    Add files via upload

--- run_simulation.py
+++ run_simulation.py
@@ -71,13 +71,15 @@ def generate_huffman_codes(root):
     generate_codes_helper(root, [])
     return codes, code_lengths
 
-
 def encode_sentence(sentence, codes):
     encoded_sentence = []
+    code_lengths = []
     words = sentence.split()
     for word in words:
         encoded_sentence.extend(codes[word])
-    return encoded_sentence
+        code_lengths.append(len(codes[word]))
+    return encoded_sentence, code_lengths
+
 
 
 def decode_sentence(encoded_sentence, root):
@@ -126,68 +128,58 @@ start_idx = 500
 end_idx = 701
 inputCode = []
 huff_length = [0]
-# #%%
+#%%
+max_w = 0
+for w in code_length_list[500:701]:
+    if len(w) > max_w:
+        max_w = len(w)
+#%%
 # huff_length = scipy.io.loadmat('huff_length_500_700.mat')['huff_length']
 # inputCode = scipy.io.loadmat('inputCodes500_700.mat')['inputCodes']
 for j in range(start_idx,end_idx):
     for b in encoded_sentences[j]:
         inputCode.append(b)
-    for bb in range(int(np.ceil(len(code_length_list[j])/2))):
-        #print(bb)
-        try:
-            hufff = code_length_list[j][bb*2] + code_length_list[j][bb*2+1]
-        except:
-            hufff = code_length_list[j][bb*2]
-        huff_length.append(hufff)
+    hufff_1 = sum(code_length_list[j][:30])
+    huff_length.append(hufff_1)
+    hufff_2 = sum(code_length_list[j][30:])
+    if hufff_2 > 0:
+        huff_length.append(hufff_2)
     huff_length.append(0)
 inputCode = np.array(inputCode).reshape(1,-1)
 huff_length  = np.array(huff_length).reshape(1,-1)
 #%%
 device ='cuda'
 #%%
-def channel_equalized_noise(noise,fading):
-    real_in1 = noise[:,:,0]
-    comp_in1 = noise[:,:,1]
-    real_in2 = fading[:,:,0]
-    comp_in2 = fading[:,:,1]
-    real_out = np.multiply(real_in1,real_in2) + np.multiply(comp_in1,comp_in2)
-    comp_out = -np.multiply(real_in1,comp_in2) + np.multiply(comp_in1,real_in2)
-    num = np.stack((real_out,comp_out),axis=2)
-    denom_s = np.multiply(real_in2,real_in2)+np.multiply(comp_in2,comp_in2)
-    denom = np.stack((denom_s,denom_s),axis=2)
-    return np.divide(num,denom)
 def rayleigh_channel(x,snr):
+    #print(x.shape)
+    n_power = 1 /(10 ** (snr / 10.0))
+    n_std = np.sqrt(n_power/2)
     x_real = tf.math.real(x)
     x_imag = tf.math.imag(x)
     x_2  = tf.stack((x_real,x_imag),axis=2)
-    print(x_2.shape)
-    fading = np.random.randn(x_2.shape[0],x_2.shape[1],x_2.shape[2])*(1/np.sqrt(2))
-    n_power = 1 /(10 ** (snr / 10.0));
-    #n_power = x_power / (10 ** (snr / 10.0))
-    n_std = np.sqrt(n_power/2)
-    noise = np.random.randn(x_2.shape[0],x_2.shape[1],x_2.shape[2]) *n_std
-    equalized = x_2 + channel_equalized_noise(noise,fading)
-    eqq = tf.complex(equalized[:,:,0],equalized[:,:,1])
-    return eqq
-# #%%
-# encoder = sionna.fec.ldpc.encoding.LDPC5GEncoder(k                 = 11, # number of information bits (input)
-#                         n                 = 33) # number of codeword bits (output)
+    
+    x_2 = tf.reshape(x_2,(x.shape[-1],2))
+    #print(x_2.shape)
+    #noise = np.random.randn(x_2.shape) *n_std
+    H_real = tf.random.normal([1], mean=0.0, stddev=np.sqrt(1/2))
+    #print(H_real.shape)
+    H_imag = tf.random.normal([1], mean=0.0, stddev=np.sqrt(1/2))
+    H = tf.stack([[H_real, -H_imag], [H_imag, H_real]], axis=0)
+    H = tf.reshape(H,(2,2))
+    Tx_sig = tf.linalg.matmul(x_2, H)
+    
+    #Rx_sig = Tx_sig + tf.random.normal((1,2),mean=0.0,stddev=n_std)
+    Rx_sig = Tx_sig + tf.random.normal(Tx_sig.shape,mean=0.0,stddev=n_std)
+    H_inv = tf.linalg.inv(H)
+    Rx_sig = tf.linalg.matmul(Rx_sig, H_inv)
+    
+    #Rx_sig = tf.reshape(Rx_sig, shape)
+    eq = tf.complex(Rx_sig[:,0],Rx_sig[:,1])
+    return tf.reshape(eq,(1,Rx_sig.shape[0]))
 
-
-# decoder = sionna.fec.ldpc.decoding.LDPC5GDecoder(encoder           = encoder,
-#                         num_iter          = 100, # number of BP iterations
-#                         return_infobits   = True)
-#%%
-encoder = sionna.fec.polar.Polar5GEncoder(k          = 13, # number of information bits (input)
-                          n          = 39) # number of codeword bits (output)
-
-
-decoder = sionna.fec.polar.Polar5GDecoder(enc_polar    = encoder, # connect the Polar decoder to the encoder
-                          dec_type   = "SCL", # can be also "SC" or "BP"
-                          list_size  = 32)
 #%%
 polar_enc_dec = {}
-for k in range(13,40):
+for k in range(13,330):
     polar_enc = sionna.fec.polar.Polar5GEncoder(k          = k, # number of information bits (input)
                               n          = int(3*k)) # number of codeword bits (output)
     polar_dec = sionna.fec.polar.Polar5GDecoder(enc_polar    = polar_enc, # connect the Polar decoder to the encoder
@@ -200,19 +192,20 @@ modulator = sionna.mapping.Mapper(constellation_type = 'qam',num_bits_per_symbol
 demodulator = sionna.mapping.Demapper(demapping_method='maxlog',constellation_type = 'qam',num_bits_per_symbol=2)
 awgn_channel = sionna.channel.AWGN()
 decoded_all_awgn = []
-for snr in range(-3,4):
+for snr in range(-3,10):
     print('awgn ',snr)
     no = 1/(10**(snr/10))
     decoded_sent_list = []
     last_bit = 0
     concat_bit = tf.cast(np.array([0]).reshape(1,1),tf.float32)
     for i in range(huff_length.shape[1]):
+        print(i)
         if huff_length[0,i] == 0:
             if i > 0:
                 
                 #print(dec_seq)
                 decoded_sent_list.append(tf.concat(dec_seq,1))
-                print(i)
+                #print(i)
             dec_seq = []
         else:
             c = inputCode[0,last_bit:last_bit+huff_length[0,i]]
@@ -271,17 +264,19 @@ for snr in range(-3,4):
                     last_bit += huff_length[0,i]
     decoded_all_awgn.append(decoded_sent_list)
 decoded_all_rayleigh = []
-for snr in range(0,13,2):
+snr_rayleigh = [-3,-1,0,2,4,6,8,10,12]
+for snr in snr_rayleigh:
     print('rayleigh ', snr)
     no = 1/(10**(snr/10))
     decoded_sent_list = []
     last_bit = 0
     concat_bit = tf.cast(np.array([0]).reshape(1,1),tf.float32)
     for i in range(huff_length.shape[1]):
+        print(i)
         if huff_length[0,i] == 0:
             if i > 0:
                 decoded_sent_list.append(tf.concat(dec_seq,1))
-                print(i)
+                #print(i)
             dec_seq = []
         else:
             c = inputCode[0,last_bit:last_bit+huff_length[0,i]]
@@ -344,7 +339,8 @@ for snr in range(0,13,2):
 #%%
 huff_decoded_sentences_awgn = []
 huff_decoded_sentences_rayleigh = []
-
+decoded_all_rayleigh = []
+decoded_all_rayleigh.append(decoded_sent_list)
 for snr in range(7):
     #for i in range(2):
         #print(i)
@@ -361,3 +357,12 @@ with open('decoded_sentences_awgn.pickle', 'wb') as handle:
     pickle.dump(huff_decoded_sentences_awgn, handle, protocol=pickle.HIGHEST_PROTOCOL)
 with open('decoded_sentences_rayleigh.pickle', 'wb') as handle:
     pickle.dump(huff_decoded_sentences_rayleigh, handle, protocol=pickle.HIGHEST_PROTOCOL)
+#%%
+total_bits = 0
+bit_error = 0
+decoded_bits = []
+for i in range(201):
+    for j in range(decoded_all_rayleigh[0][i].shape[1]):
+        decoded_bits.append(int(np.array(decoded_all_rayleigh[0][i])[0,j]))
+#%%
+print(np.sum(np.array(decoded_bits)!=inputCode))
